2b77b7634bfd836df88a1aeabf7f4dd36b301e00
Nathan Anthony <nsanthony95@gmail.com>
2022-05-11 16:03:07 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: `Launch Countdown Timer`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap"]
+};
